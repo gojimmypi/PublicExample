9d825fbacf8dac51b4d47d304815e6720e54d808
gojimmypi <gojimmypi@gmail.com>
2016-11-06 09:12:11 -0800

    add details of private settings

--- PublicExample.ino
+++ PublicExample.ino
@@ -1,5 +1,25 @@
 #include "/workspace/myPrivateSettingsTest.h"
 
+// either create your own private settings file, or uncomment and use these:
+//const char* WIFI_SSID = "mySSID";
+//const char* WIFI_PWD = "mySSID Password";
+
+// TimeClient settings
+//const float UTC_OFFSET = -7;
+
+// Wunderground Settings
+//const boolean IS_METRIC = false;
+//const String WUNDERGRROUND_API_KEY = "my key";
+//const String WUNDERGRROUND_LANGUAGE = "EN";
+//const String WUNDERGROUND_COUNTRY = "CA";
+//const String WUNDERGROUND_CITY = "my City";
+
+//Thingspeak Settings
+//const String THINGSPEAK_CHANNEL_ID = "my ID";
+//const String THINGSPEAK_API_READ_KEY = "my Key";
+
+
+
 void setup()
 {
 
