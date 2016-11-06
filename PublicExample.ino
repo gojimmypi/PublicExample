12d46176420c1bf5118db8eaf2bbc1329215ae64
gojimmypi <gojimmypi@gmail.com>
2016-11-06 09:06:49 -0800

    added a third line of text, and reference to myPrivateSettingsTest

--- PublicExample.ino
+++ PublicExample.ino
@@ -1,3 +1,5 @@
+#include "/workspace/myPrivateSettingsTest.h"
+
 void setup()
 {
 
@@ -9,6 +11,7 @@ void loop()
 {
 	Serial.println("Hello World!");
 	Serial.println("More text here...");
+	Serial.println("la la la...");
 
   /* add main program code here */
 
